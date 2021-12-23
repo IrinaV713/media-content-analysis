f41ab23abaaa66690ce0ac664d4fb3640e0be1dd
IrinaV713 <76870532+IrinaV713@users.noreply.github.com>
2021-12-23 12:28:39 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    DATABASE_NAME: 'Media content analysis system',
+    DATABASE_USERNAME: 'root',
+    DATABASE_PASSWORD: '',
+    DATABASE_HOST: 'localhost',
+    DATABASE_DRIVER: 'mysql',
+}
